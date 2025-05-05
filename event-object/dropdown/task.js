32fa6b2216fcb2a66ab76dd9bbe3c1b119a8d1f9
Peter <turok3445@mail.ru>
2025-05-05 19:47:18 +0300

    update event-object/dropdown/task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+let dropDownValue = Array.from(document.querySelectorAll(".dropdown__value"));
+let link = Array.from(document.querySelectorAll(".dropdown__item"))
+
+
+dropDownValue.forEach((element)=> {
+    element.addEventListener("click", () => {
+        console.log("222")
+        element.closest(".dropdown").querySelector(".dropdown__list").classList.toggle("dropdown__list_active")
+        event.preventDefault();
+    })})
+    
+    link.forEach ((element) => {
+        element.addEventListener("click", () => {
+            element.closest(".dropdown").querySelector(".dropdown__value").textContent = element.querySelector(".dropdown__link").textContent;
+            console.log(element.querySelector(".dropdown__link").textContent)
+            element.closest(".dropdown").querySelector(".dropdown__list").classList.remove("dropdown__list_active")
+            event.preventDefault()
+        })
+    })
