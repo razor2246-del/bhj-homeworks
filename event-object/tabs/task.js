55ec0600bc35f5c18e22be8ce6e79932ffc898ab
Peter <turok3445@mail.ru>
2025-05-06 21:10:20 +0300

    update event-object/dropdown/task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,6 +3,13 @@ let tabContent = Array.from(document.querySelectorAll(".tab__content"));
 
 tab.forEach ((e, index) => {
     e.addEventListener("click", () => {
-        e.classList.toggle("tab_active")
+        e.closest(".tabs").querySelectorAll(".tab").forEach((tab) => {
+            tab.classList.remove("tab_active")
+        })
+        e.classList.add("tab_active")
+        e.closest(".tabs").querySelectorAll(".tab__content").forEach((el) => {
+            el.classList.remove("tab__content_active")
+        })
+        tabContent[index].classList.add("tab__content_active")
     })
 })
